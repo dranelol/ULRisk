dcf51c651d7f7f1a9c98489ed700bb9048961ac4
dranelol <cardsfan853@gmail.com>
2016-10-04 19:49:19 -0500

    l

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -1,15 +1,59 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
-public class MapManager : MonoBehaviour {
+public class MapManager : MonoBehaviour
+{
+    [Serializable]
+    public class MapRegionDatabase : SerializedDictionary<int, GameObject> { }
 
-	// Use this for initialization
-	void Start () {
+    /// <summary>
+    /// record of user stats for this session
+    /// </summary>
+    [SerializeField]
+    public MapRegionDatabase MapRegions = new MapRegionDatabase();
+
+    private static MapManager instance;
+    public static MapManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = GameObject.FindObjectOfType<MapManager>();
+            }
+            DontDestroyOnLoad(instance.gameObject);
+            return instance;
+        }
+    }
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            //If I am the first instance, make me the Singleton
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            //If a Singleton already exists and you find
+            //another reference in scene, destroy it!
+            if (this != instance)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+
+    }
+
+    void Start ()
+    {
 	
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 	
 	}
 }
